572d5b01c0b13001c2cf02d9b872d0f0423cebdc
nimilitao <nicolasmilivotto@gmai.com>
2022-07-26 22:30:06 -0300

    event handling

--- js/script.js
+++ js/script.js
@@ -3,33 +3,64 @@ const pokemonNumber = document.querySelector('.pokemonNumber');
 const pokemonImage = document.querySelector('.pokemonImg');
 
 const form = document.querySelector('#form');
-const Search = document.querySelector('.InputSearch');
+const input = document.querySelector('.inputSearch');
+const btnPrev = document.querySelector('.btnPrev');
+const btnNext = document.querySelector('.btnNext');
+
+let searchPokemon  =1;
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`); //promise
+    console.log(APIResponse);
     
-    const data = await APIResponse.json(); // função assíncrona: leva um tempo para executar
-
-    console.clear();
-    console.log(data)
-    return data;
+    if (APIResponse.status == 200) {
+        const data = await APIResponse.json(); // função assíncrona: leva um tempo para executar
+        return data;
+    }
 }
 
 const renderPokemon = async (pokemon) =>{
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
     const data = await fetchPokemon(pokemon);
 
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-    // não passamos à propriedade com "." pois temos na api a chave "generation-i" onde daria erro, pois o vsCode apontaria o "-i" como fora da chave
-    console.log(data)
+    if(data){ // se tiver algo no data
+        pokemonImage.style.display = 'block';
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        // não passamos à propriedade com "." pois temos na api a chave "generation-i" onde daria erro, pois o vsCode apontaria o "-i" como fora da chave
+        searchPokemon = data.id;
+        input.value = "";
+
+        console.log(data)
+    }
+    else{
+        pokemonImage.style.display = 'none'; 
+        pokemonName.innerHTML = 'Not found';
+        pokemonNumber.innerHTML = '404'
+    }
 }
 
 form.addEventListener('submit', (event) =>{
     event.preventDefault();
 
-    console.log('enviando formulário...')
-    console.log(Search);
+    console.clear();
+    renderPokemon(input.value.toLowerCase());
 });
 
-renderPokemon('1');
+btnPrev.addEventListener('click', () =>{
+    if(searchPokemon > 1)
+    {
+        searchPokemon--;
+        renderPokemon(searchPokemon);
+    }
+});
+
+btnNext.addEventListener('click', () =>{
+    searchPokemon++;
+    renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
